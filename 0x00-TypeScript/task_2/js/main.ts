2fd5a7a396ff31586e9fb830de151a20eeea1282
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-02 04:06:37 +0000

    added first iteration of task 2 main.ts

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -1,102 +1,51 @@
-// Task(1 - 4)
+// Task(2)
 
-/* Function Interfaces */
-interface printTeacherFunction {
-    (firstName: string, lastName: string): string, 
-}
+/* Director Interface & Class */
+interface DirectorInterface {
+    workFromHome(): string,
+    getCoffeeBreak(): string,
+    workDirectorTasks(): string,
+} 
 
-/* Functions */
-function printTeacher(firstName: string, lastName: string):string {
-    return (`${firstName[0]}. ${lastName}`);
-}
-
-/* Class interfaces */
-type homeworkStatusRes = "Currently working"
-
-interface studentClassContract {
-    firstName: string,
-    lastName: string,
-    workOnHomework: () => homeworkStatusRes,
-    displayName: () => string,
-}
-
-/* Classes */
-class StudentClass implements studentClassContract{
-    firstName: string;
-    lastName: string;
-   
-    constructor(firstName: string, lastName: string) {
-      this.firstName = firstName;
-      this.lastName = lastName;
+class Director implements DirectorInterface {
+    workFromHome() {
+        return ('Working from home');
     }
-
-    workOnHomework():homeworkStatusRes {
-        return ("Currently working");
+    getCoffeeBreak() {
+        return ('Getting a coffee break');
     }
-
-    displayName():string {
-        return (this.firstName);
+    workDirectorTasks() {
+        return ('Getting to director tasks');
     }
-  }
-
-/* Regular Interfaces */
-interface Teacher {
-    firstName: string,
-    lastName: string,
-    fullTimeEmployee: boolean,
-    yearsOfExperience?: number, // Option yearsOfExperience
-    location: string,
-    [key: string]: any, // Optional key / value
 }
 
-interface Directors extends Teacher{
-    numberOfReports: number,
+/* Teacher Interface & Class */
+interface TeacherInterface{
+    workFromHome(): string,
+    getCoffeeBreak(): string,
+    workTeacherTasks(): string,
 }
 
-/* Test code ===============================*/
-const teacherNoOptional: Teacher = {
-    firstName: 'John',
-    lastName: 'Doe',
-    fullTimeEmployee: true,
-    location: 'Connecticut'
-};
+class Teacher implements TeacherInterface {
+    workFromHome() {
+        return ('Cannot work from home');
+    }
+    getCoffeeBreak() {
+        return ('Cannot have a break');
+    }
+    workTeacherTasks() {
+        return ('Getting to work');
+    }
+}
 
-const teacherOneOptional: Teacher = {
-    firstName: 'Rock',
-    lastName: 'Stone',
-    fullTimeEmployee: false,
-    yearsOfExperience: 8,
-    location: 'Florida'
-};
+function createEmployee(salary: number | string):Director | Teacher {
+    if (salary < 500) {
+        return (new Teacher());
+    }
+    return (new Director());
+}
 
-const teacherTwoOptional: Teacher = {
-    firstName: 'Jack',
-    lastName: 'stand',
-    fullTimeEmployee: false,
-    yearsOfExperience: 8,
-    location: 'Montana',
-    contract: false,
-};
 
-const directorTest: Directors = {
-    firstName: 'Santa',
-    lastName: 'Claus',
-    location: 'The North Pole',
-    fullTimeEmployee: true,
-    numberOfReports: 17,
-};
-
-console.log("Teacher object with not optional keys", teacherNoOptional);
-console.log("Teacher object with one optional key: key = yearsOfExperience", teacherOneOptional);
-console.log("Teacher object with two optional keys: (key = yearsOfExperience & key(random) = value(random))", teacherTwoOptional);
-console.log("Director test: ", directorTest);
-
-let testPrintTeacher: printTeacherFunction = printTeacher;
-console.log(testPrintTeacher(directorTest.firstName, directorTest.lastName));
-
-let testClass = new StudentClass("John", "doe");
-
-console.log("(StudentClasstest) created new StudentClass instance: firstName = John, lastName = doe")
-
-console.log("(StudentClasstest) testing displayName() function - Output:", testClass.displayName());
-console.log("(StudentClasstest) test workOnHomework() function - Output:", testClass.workOnHomework());
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
