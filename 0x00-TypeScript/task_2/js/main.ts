b794aa87512ee64062ee58cfcb174521308dbedb
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-03 00:24:05 +0000

    added two new functions to main.ts

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -8,13 +8,13 @@ interface DirectorInterface {
 } 
 
 class Director implements DirectorInterface {
-    workFromHome() {
+    workFromHome(): string {
         return ('Working from home');
     }
-    getCoffeeBreak() {
+    getCoffeeBreak(): string {
         return ('Getting a coffee break');
     }
-    workDirectorTasks() {
+    workDirectorTasks(): string {
         return ('Getting to director tasks');
     }
 }
@@ -27,25 +27,41 @@ interface TeacherInterface{
 }
 
 class Teacher implements TeacherInterface {
-    workFromHome() {
+    workFromHome(): string {
         return ('Cannot work from home');
     }
-    getCoffeeBreak() {
+    getCoffeeBreak(): string {
         return ('Cannot have a break');
     }
-    workTeacherTasks() {
+    workTeacherTasks(): string {
         return ('Getting to work');
     }
 }
 
-function createEmployee(salary: number | string):Director | Teacher {
+function createEmployee(salary: number | string): Director | Teacher {
     if (salary < 500) {
         return (new Teacher());
     }
     return (new Director());
 }
 
+function isDirector(employee: any): employee is Director {
+    return typeof employee.workDirectorTasks === 'function';
+}
+
+function executeWork(employee: any): string {
+    if (isDirector(employee) === false) {
+        return (employee.workTeacherTasks());
+    } else {
+        return (employee.workDirectorTasks());
+    }
+}
+
 
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
+
+console.log(executeWork(createEmployee(200)));
+
+console.log(executeWork(createEmployee(1000)));
