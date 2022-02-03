095455a415eead2dd4009e2f02d631ac3691c711
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-03 05:18:05 +0000

    added first namespace called 'subjects' with a teacher object interface

---
+++ 0x00-TypeScript/task_4/js/subjects/Teacher.ts
@@ -0,0 +1,8 @@
+// Teacher interface in a namespace named Subjects ( Namespace & Declaration merging )
+
+namespace Subjects {
+    export interface Teacher {
+        firstName: string,
+        lastName: string,
+    }
+}
