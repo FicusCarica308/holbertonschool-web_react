229a7f084edf57cf9a6120e1cf35c35de802beb8
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-03 02:13:35 +0000

    added task_3 directory with config files

---
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -0,0 +1,83 @@
+// Task(2)
+
+/* Director Interface & Class */
+interface DirectorInterface {
+    workFromHome(): string,
+    getCoffeeBreak(): string,
+    workDirectorTasks(): string,
+} 
+
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return ('Working from home');
+    }
+    getCoffeeBreak(): string {
+        return ('Getting a coffee break');
+    }
+    workDirectorTasks(): string {
+        return ('Getting to director tasks');
+    }
+}
+
+/* Teacher Interface & Class */
+interface TeacherInterface{
+    workFromHome(): string,
+    getCoffeeBreak(): string,
+    workTeacherTasks(): string,
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return ('Cannot work from home');
+    }
+    getCoffeeBreak(): string {
+        return ('Cannot have a break');
+    }
+    workTeacherTasks(): string {
+        return ('Getting to work');
+    }
+}
+
+/* Teacher and Director functions */
+
+function createEmployee(salary: number | string): Director | Teacher {
+    if (salary < 500) {
+        return (new Teacher());
+    }
+    return (new Director());
+}
+
+function isDirector(employee: any): employee is Director {
+    return typeof employee.workDirectorTasks === 'function';
+}
+
+function executeWork(employee: any): string {
+    if (isDirector(employee) === false) {
+        return (employee.workTeacherTasks());
+    } else {
+        return (employee.workDirectorTasks());
+    }
+}
+
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
+
+/* ======== END */
+
+/* String literal types */
+type Subjects = 'Math' | 'History'
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return ('Teaching Math')
+    } else if (todayClass === 'History') {
+        return ('Teaching History')
+    }
+}
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
