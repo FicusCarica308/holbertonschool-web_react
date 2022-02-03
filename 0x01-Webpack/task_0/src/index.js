f89188c554545325fc7913077fbd3d736ca3d1e8
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-03 23:45:50 +0000

    added task_0 files

---
+++ 0x01-Webpack/task_0/src/index.js
@@ -0,0 +1,11 @@
+const $ = require( "jquery" );
+
+function makeParagraph (content) {
+  $("<p>")
+    .html(content)
+    .appendTo('body')
+}
+
+makeParagraph('Holberton Dashboard');
+makeParagraph('Dashboard data for the students');
+makeParagraph('Copyright - Holberton School');
