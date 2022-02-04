485ff1faedc703a668c66595de90474e3c87e996
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-04 17:43:20 +0000

    added header directory

---
+++ 0x01-Webpack/task_3/header/header.js
@@ -0,0 +1,6 @@
+const $ = require( "jquery" );
+import './header.css';
+
+$('body').append('<header></header>');
+$('header').append('<div id=logo></div>');
+$('header').append('<p>Holberton Dashboard</p>');
