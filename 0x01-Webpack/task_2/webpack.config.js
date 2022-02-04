3b37fb94ec1bd356ef06a833ca44af02157deacc
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-04 05:11:31 +0000

    reverted changes

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -23,6 +23,18 @@ module.exports = {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
         type: 'asset/resource',
       },
+      {
+      use: [
+        'file-loader',
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            bypassOnDebug: true, // webpack@1.x
+            disable: true, // webpack@2.x and newer
+          },
+        },
+      ],
+    }
     ]
   },
 };
