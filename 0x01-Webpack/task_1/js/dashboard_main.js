2105ad763ba9c3d8ce72650f571a5559bf8e9d0b
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-04 03:11:50 +0000

    added leading and trailing options

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,8 +1,6 @@
 const $ = require( "jquery" );
 const _ = require('lodash');
 
-let count = 0;
-
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>')
@@ -10,8 +8,12 @@ $('body').append('<p id=count></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
 function updateCounter() {
-  count += 1;
-  $('#count').html(`${count} clicks on the button`)
+  let count = 0;
+
+  $('button').on('click', _.debounce(() => {
+    count += 1;
+    $('#count').html(`${count} clicks on the button`)
+  }, 500, {leading: true, trailing: false,}));
 }
 
-$('button').on('click', _.debounce(updateCounter, 500));
+updateCounter();
