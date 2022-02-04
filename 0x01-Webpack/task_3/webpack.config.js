52fe7cf05c7037472a35f56803bf718c99a8476a
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-04 17:06:30 +0000

    added 'mode' change and devServer key

---
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
+  },
+  performance: {
+      maxAssetSize: 5000000
+  },  
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devServer: {
+    port: 8564,
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.css$/, 
+        use: ["style-loader", "css-loader"] 
+      },
+      {
+        test: /\.()$/i,
+        use: ["file-loader",
+          {
+            loader: "image-webpack-loader",
+          }
+        ]
+      }
+    ]
+  },
+};
