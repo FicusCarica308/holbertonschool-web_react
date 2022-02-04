48d5ffd4b26d44beb2b1bac70e317df72abfab6d
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-04 18:06:48 +0000

    added evyerthing for body

---
+++ 0x01-Webpack/task_3/modules/body/body.js
@@ -0,0 +1,17 @@
+const _ = require('lodash');
+const $ = require( "jquery" );
+
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>')
+$('body').append('<p id=count></p>');
+
+function updateCounter() {
+  let count = 0;
+
+  $('button').on('click', _.debounce(() => {
+    count += 1;
+    $('#count').html(`${count} clicks on the button`)
+  }, 500));
+}
+
+updateCounter();
