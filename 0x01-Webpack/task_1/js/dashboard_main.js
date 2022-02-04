1f74efaedc7c38c9f966c19b2537ce276bf4ad65
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-04 02:58:44 +0000

    moved debounce out of updateCounter function
    breaks functionality

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -4,16 +4,17 @@ const _ = require('lodash');
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>')
-$('body').append('<p id=\'count\'></p>');
+$('body').append('<p id=count></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
 function updateCounter() {
   let count = 0;
 
-  $('button').on('click', _.debounce(() => {
+  $('button').on('click', () => {
     count += 1;
     $('#count').html(`${count} clicks on the button`)
-  }, 500));
+  });
 }
 
-updateCounter();
+const debounced = _.debounce(updateCounter, 1000);
+debounced();
