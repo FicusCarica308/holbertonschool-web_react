ac28634f926ce0a7ae94f61c1c7593e9ade1f448
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-04 02:34:54 +0000

    added working updateCounter function

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,7 +1,20 @@
 const $ = require( "jquery" );
+const _ = require('lodash');
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>')
 $('body').append('<p id=count></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
+
+function updateCounter() {
+  let count = 0;
+
+  function dbounced () {
+    count += 1;
+    $('#count').html(`${count} clicks on the button`)
+  }
+  $('button').on('click', _.debounce(dbounced, 500));
+}
+
+updateCounter();
