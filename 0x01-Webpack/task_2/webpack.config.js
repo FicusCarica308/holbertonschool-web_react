24314bb9e36870c7950e36288f2354b31e39f077
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-04 04:56:01 +0000

    used file-loader instead of asset/resource

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -19,10 +19,19 @@ module.exports = {
         test: /\.css$/, 
         use: ["style-loader", "css-loader"] 
       },
-      { 
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
-      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      }
     ]
   },
 };
