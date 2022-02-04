3de10fe6dfc23f921367199cba6d3eeb08c131ad
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-04 03:16:43 +0000

    changing order of code to see if it helps my error

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,12 +1,6 @@
 const $ = require( "jquery" );
 const _ = require('lodash');
 
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>')
-$('body').append('<p id=count></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
-
 function updateCounter() {
   let count = 0;
 
@@ -16,4 +10,9 @@ function updateCounter() {
   }, 500));
 }
 
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>')
 updateCounter();
+$('body').append('<p id=count></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
