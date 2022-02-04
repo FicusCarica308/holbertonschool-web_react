fbd7edc559e3dce20acd0b95603adfabcc3f23d7
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-04 18:06:35 +0000

    added body entry

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -5,11 +5,12 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: {
-    all: path.resolve(__dirname, './modules/header/header.js'),
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
   },
   performance: {
       maxAssetSize: 5000000
-  },  
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'public')
