038a84715306ff5a57565daf5e995c17a21ac914
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-04 03:09:36 +0000

    made count a global and moved debounce outside of updateCounter

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,6 +1,8 @@
 const $ = require( "jquery" );
 const _ = require('lodash');
 
+let count = 0;
+
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>')
@@ -8,12 +10,8 @@ $('body').append('<p id=count></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
 function updateCounter() {
-  let count = 0;
-
-  $('button').on('click', _.debounce(() => {
-    count += 1;
-    $('#count').html(`${count} clicks on the button`)
-  }, 500));
+  count += 1;
+  $('#count').html(`${count} clicks on the button`)
 }
 
-updateCounter();
+$('button').on('click', _.debounce(updateCounter, 500));
