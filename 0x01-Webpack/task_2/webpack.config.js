e00f0afedcf56f856ff1438ade2146768e7f511e
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-04 05:40:36 +0000

    added new version of config file

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
   performance: {
       maxAssetSize: 5000000
   },  
-
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
@@ -19,18 +18,10 @@ module.exports = {
         test: /\.css$/, 
         use: ["style-loader", "css-loader"] 
       },
-      { 
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
-            },
-          },
-        ],
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        loader: 'image-webpack-loader',
+        enforce: 'pre'
       }
     ]
   },
