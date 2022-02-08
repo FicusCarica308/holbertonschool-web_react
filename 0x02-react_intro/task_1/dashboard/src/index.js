68b717716fe8ac7f9a31af48ca2169b2c13ff1a9
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-08 06:36:12 +0000

    added notifications feature

--- 0x02-react_intro/task_1/dashboard/src/index.js
+++ 0x02-react_intro/task_1/dashboard/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import App from './App';
+import Notifications from './Notifications'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <root-notifications>
+      <Notifications />
+    </root-notifications>
   </React.StrictMode>,
   document.getElementById('root')
 );
