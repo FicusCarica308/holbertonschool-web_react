7c0422909675d6f8c2a3cc82337b0c243507ba26
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-11 20:34:33 +0000

    added content back to webpack.config

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -3,4 +3,36 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    hot: true,
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.css$/, 
+        use: ["style-loader", "css-loader"] 
+      },
+      {
+        test: /\.()$/i,
+        use: ["file-loader",
+          {
+            loader: "image-webpack-loader",
+          }
+        ]
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin()
+  ],
 };
