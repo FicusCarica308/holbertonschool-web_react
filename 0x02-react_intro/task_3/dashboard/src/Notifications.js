f56d7b907843d153bc43cd5d8e53a0007f5ed7e7
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-11 15:59:49 +0000

    added opty of task_2 directory with a new file called utils.test.js

---
+++ 0x02-react_intro/task_3/dashboard/src/Notifications.js
@@ -0,0 +1,39 @@
+/* Notifications element for App.js... */
+import closeIcon from './close-icon.png';
+import React from 'react';
+import './Notifications.css'
+import {getLatestNotification} from './utils.js';
+
+export default function Notifications() {
+  return (
+    <div className='Notifications'>
+      <p>Here is the list of notifications</p>
+      <ul className='notification-list'>
+        <li data-priority='default'>New course available</li>
+        <li data-priority='urgent'>New resume available</li>
+        <li 
+          data-priority='urgent'
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        >
+        </li>
+      </ul>
+      <button
+          aria-label="Close"
+          onClick={() => {
+            console.log('Close button has been clicked');
+          }}
+          style={{
+            position: 'absolute',
+            right: '20px',
+            top: '15px',
+            width: '20px',
+            height: '20px',
+            background: 'none',
+            border: 'none'
+          }}
+        >
+        <img src={closeIcon} alt='closeIcon' style={{width: '20px', height: '20px',}} />
+      </button>
+    </div>
+  );
+}
