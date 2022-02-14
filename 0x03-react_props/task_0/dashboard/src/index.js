7a2a81ec8b8444c75b99774ec3b653363243b6fa
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-14 09:24:26 +0000

    added starter files for task_0

---
+++ 0x03-react_props/task_0/dashboard/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App/App.css';
+import App from './App/App';
+import Notifications from './Notifications/Notifications'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <root-notifications>
+      <Notifications />
+    </root-notifications>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
