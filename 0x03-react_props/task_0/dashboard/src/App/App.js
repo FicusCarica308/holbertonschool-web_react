f63895b0b06017277bef95b260265e152761d657
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-14 10:13:39 +0000

    made App.js the main entry point of the project
    split App.js jsx into different components each with its own corrisponding directory in src/

--- 0x03-react_props/task_0/dashboard/src/App/App.js
+++ 0x03-react_props/task_0/dashboard/src/App/App.js
@@ -1,29 +1,25 @@
 import React from 'react';
-import holbertonLogo from '../assets/holberton-logo.jpg';
 import './App.css';
-import {getFullYear, getFooterCopy} from '../utils/utils.js';
+import Notifications from '../Notifications/Notifications'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+import Login from '../Login/Login'
 
 function App() {
   return (
-    <div className="App">
-      <div className='App-header'>
-        <img src={holbertonLogo} className='holberton-logo' alt='logo' />
-        <h1>School dashboard</h1>
+    <React.Fragment>
+      <root-notifications>
+        <Notifications />
+      </root-notifications>
+      <div className="App">
+        <Header />
+        <div className='App-body'>
+          <p>Login to access the full dashboard</p>
+          <Login />
+        </div>
+        <Footer />
       </div>
-      <div className='App-body'>
-        <p>Login to access the full dashboard</p>
-        <form className='login-form'>
-          <label htmlFor='email' id='email-label'>Email:</label>
-          <input type='email' name='email' id='email'></input>
-          <label htmlFor='password' id='password-label'>Password:</label>
-          <input type='text' name='password' id='password'></input>
-          <button>OK</button>
-        </form>
-      </div>
-      <div className='App-footer'>
-        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-      </div>
-    </div>
+    </React.Fragment>
   );
 }
 
--- 0x03-react_props/task_0/dashboard/src/index.js
+++ 0x03-react_props/task_0/dashboard/src/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App/App.css';
 import App from './App/App';
-import Notifications from './Notifications/Notifications'
 
 ReactDOM.render(
   <React.StrictMode>
-    <root-notifications>
-      <Notifications />
-    </root-notifications>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
