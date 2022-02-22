e583d69176326093158eac6b059ac3f9af08c492
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-22 17:37:45 +0000

    swapped App function in App.js to React.Componenet

--- 0x04-React_component/task_0/dashboard/src/App/App.js
+++ 0x04-React_component/task_0/dashboard/src/App/App.js
@@ -8,39 +8,42 @@ import Login from '../Login/Login'
 import CourseList from '../CourseList/CourseList'
 import PropTypes from 'prop-types';
 
-function App({isLoggedIn}) {
-  const listCourses = [
-    {id: 1, name:"ES6", credit: 60},
-    {id: 2, name:"Webpack", credit: 20},
-    {id: 3, name:"React", credit: 40}
-  ]
+const listCourses = [
+  {id: 1, name:"ES6", credit: 60},
+  {id: 2, name:"Webpack", credit: 20},
+  {id: 3, name:"React", credit: 40}
+]
 
-  const listNotifications = [
-    {id: 1, type: 'default', value: 'New course available'},
-    {id: 2, type: 'urgent',  value: 'New resume available'},
-    {id: 3, type: 'urgent', html: {__html: getLatestNotification()}}
-  ]
-  return (
-    <React.Fragment>
-      <div className="App">
-      <root-notifications>
-        <Notifications listNotifications={[]}/>
-      </root-notifications>
-        <Header />
-        <div className='App-body'>
-          {isLoggedIn === false ? (
-            <React.Fragment>
-              <p>Login to access the full dashboard</p>
-              <Login />
-            </React.Fragment>
-          ) : (
-            <CourseList listCourses={listCourses}/>
-          )}
+const listNotifications = [
+  {id: 1, type: 'default', value: 'New course available'},
+  {id: 2, type: 'urgent',  value: 'New resume available'},
+  {id: 3, type: 'urgent', html: {__html: getLatestNotification()}}
+]
+
+class App extends React.Component {
+  render() {
+    return(
+      <React.Fragment>
+        <div className="App">
+        <root-notifications>
+          <Notifications listNotifications={[]}/>
+        </root-notifications>
+          <Header />
+          <div className='App-body'>
+            {this.props.isLoggedIn === false ? (
+              <React.Fragment>
+                <p>Login to access the full dashboard</p>
+                <Login />
+              </React.Fragment>
+            ) : (
+              <CourseList listCourses={listCourses}/>
+            )}
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  }
 }
 
 App.propTypes = {
