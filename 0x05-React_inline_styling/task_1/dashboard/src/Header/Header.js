30d28e7b51d1f3faea4043b9c0a3c9d933b2e941
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-03-02 00:00:52 +0000

    changed names for all three styles on file for header

--- 0x05-React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ 0x05-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -4,25 +4,25 @@ import { StyleSheet, css } from 'aphrodite';
 import holbertonLogo from '../assets/holberton-logo.jpg';
 
 const styles = StyleSheet.create({
-  headerStyle: {
+  AppHeader: {
     display: 'flex',
     alignItems: 'center',
     borderBottom: '3px solid rgb(207, 8, 8)',
   },
-  headerHStyle: {
+  header: {
     color: 'rgb(207, 8, 8)',
     fontWeight: '660',
   },
-  logo: {
+  AppLogo: {
     width: '200px',
   }
 });
 
 export default function Header() {
   return (
-    <div className={'App-header ' + css(styles.headerStyle)}>
-      <img src={holbertonLogo} className={'holberton-logo ' + css(styles.logo)} alt='logo' />
-      <h1 className={css(styles.headerHStyle)}>School dashboard</h1>
+    <div className={'App-header ' + css(styles.AppHeader)}>
+      <img src={holbertonLogo} className={'AppLogo ' + css(styles.AppLogo)} alt='logo' />
+      <h1 className={css(styles.header)}>School dashboard</h1>
     </div>
   );
 }
