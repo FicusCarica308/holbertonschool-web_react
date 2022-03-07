06618c3ed8fd3cba12af564ffa4f9dd123cf1dbe
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-03-07 22:59:20 +0000

    added task 2 accessImmutableObject() function

---
+++ 0x07-React_Immutable/2-nested.js
@@ -0,0 +1,16 @@
+/*
+* Task 2 - Accessing nested elements
+* Given the function below, edit it to return the value of the object at the defined path
+*/
+import { getIn } from 'immutable'
+
+function accessImmutableObject(object, array) {
+  return(getIn(object, array));
+}
+
+// console.log(
+//   accessImmutableObject(
+//     {name: {first: "Guillaume", last: "Salva"}},
+//     ['name', 'first']
+//   )
+// );
