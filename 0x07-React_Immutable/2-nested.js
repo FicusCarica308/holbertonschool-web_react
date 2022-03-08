0867a1e0460feb0a2c3d0215491f2a595191ed07
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-03-08 20:44:49 +0000

    converted return value to immutable before return

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -2,17 +2,17 @@
   *Task 2 - Accessing nested elements
   *Given the function below, edit it to return the value of the object at the defined path
 */
-import { getIn } from 'immutable';
+import { getIn, fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
   return(
-    getIn(fromJS(object), array)
+    fromJS(getIn(object, array))
   );
 }
 
-// console.log(
-//   accessImmutableObject(
-//     {name: {first: "Guillaume", last: "Salva"}},
-//     ['name', 'first']
-//   )
-// );
+console.log(
+  accessImmutableObject(
+    {name: {first: "Guillaume", last: "Salva"}},
+    ['name', 'first']
+  )
+);
