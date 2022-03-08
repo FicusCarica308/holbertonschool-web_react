39da477d0613703bd8ed3b78e5eb7a54893e165b
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-03-08 21:01:56 +0000

    removed all comments from file(temporary)

--- 0x07-React_Immutable/3-list.js
+++ 0x07-React_Immutable/3-list.js
@@ -1,33 +1,13 @@
-/*
-  *Task 3 - List and push
-  *Two functions that cover immutable lists
-*/
 import { List } from 'immutable';
 
-/*
-  *getListObject - accepts an array as parameter and converts
-  *it into an immutable List using the Immutable.js library
-*/
 export function getListObject(array) {
   return (
     List(array)
   );
 }
 
-/*
-  *addElementToList - appends a string to a list and returns it
-  *Arguments: list(type: List), element(type: String)
-*/
 export function addElementToList(list, element) {
   return (
     list.push(element)
   );
 }
-
-// const testArray = ['name', 'first'];
-// let newList = undefined;
-// 
-// newList = getListObject(testArray);
-// console.log(testArray, '\n', newList);
-// 
-// console.log(addElementToList(newList, 'junk'))
