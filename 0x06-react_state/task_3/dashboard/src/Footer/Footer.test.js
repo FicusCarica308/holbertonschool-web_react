e5854c6a33b32eddb00ed91095afd43af05862ce
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-03-19 15:42:29 +0000

    added two tests to Footer.test.js in task_3
    Test to check if newly added <a> tag in footer is displayed or not displayed based on the wrapped context

--- 0x06-react_state/task_3/dashboard/src/Footer/Footer.test.js
+++ 0x06-react_state/task_3/dashboard/src/Footer/Footer.test.js
@@ -5,21 +5,40 @@ import { mount } from 'enzyme';
 import Footer from './Footer';
 import React from 'react';
 import { StyleSheetTestUtils } from 'aphrodite';
+import { AppContext, logOut } from '../App/AppContext'
 
 let wrapper = null;
+const user = {
+  email: 'John@gmail.com',
+  password: 'Doe123',
+  isLoggedIn: true
+};
 StyleSheetTestUtils.suppressStyleInjection();
 
-beforeEach(() => {
-  wrapper = mount(<Footer />);
-});
+describe('Checks if footer elements are displayed without error', () => {
+  beforeEach(() => {
+    wrapper = mount(<Footer />);
+  });
 
-
-describe('Footer Prop', () => {
   it("Checks if the Footer component is rendered properly without error", () => {
     expect(wrapper.exists('.App-footer')).toBeTruthy()
   });
 
   it("Checks if the component at the very least renders the text “Copyright”", () => {
     expect(wrapper.find('.App-footer p').text().includes('Copyright')).toBeTruthy();
+  });
+});
+
+describe('Checks if the <a> element works with different loggedin states', () => {
+  it('link is not displayed when the user is logged out within the context', () => {
+    user.isLoggedIn = false;
+    wrapper = mount(<AppContext.Provider value={{user, logOut}}><Footer /></AppContext.Provider>);
+    expect(wrapper.exists('.App-footer a')).toBeFalsy();
+  });
+
+  it('link is displayed when the user is logged in within the context', () => {
+    user.isLoggedIn = true;
+    wrapper = mount(<AppContext.Provider value={{user, logOut}}><Footer /></AppContext.Provider>);
+    expect(wrapper.exists('.App-footer a')).toBeTruthy();
   });
 });
