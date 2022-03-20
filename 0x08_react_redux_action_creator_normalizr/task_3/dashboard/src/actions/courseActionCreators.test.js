253ccc3f2b6644f172a77388118810fac4da1004
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-03-20 03:27:03 +0000

    removed random console.log() function calls

--- 0x08_react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.test.js
+++ 0x08_react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.test.js
@@ -3,14 +3,12 @@ import { SELECT_COURSE, UNSELECT_COURSE } from './courseActionTypes'
 
 describe('Tests for selectCourse() function', () => {
   it('Should return a complete Redux Action object with a index passed', () => {
-    console.log
     expect(selectCourse(1)).toEqual({ type: SELECT_COURSE, index: 1});
   });
 });
 
 describe('Tests for  unselectCourse() function', () => {
   it('Should return a complete Redux Action object with a index passed', () => {
-    console.log
     expect(unselectCourse(1)).toEqual({ type: UNSELECT_COURSE, index: 1});
   });
 });
